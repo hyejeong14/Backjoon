e88a74aebceb79a029e064b1ad3ff23a25888940
hyejeong14 <70855157+hyejeong14@users.noreply.github.com>
2023-02-28 16:50:04 +0900

    230228

---
+++ 10869.py
@@ -0,0 +1,8 @@
+num1,num2=map(int,input().split())
+while num1>=1&num2<=10000:
+    print(num1+num2)
+    print(num1-num2)
+    print(num1*num2)
+    print(num1//num2)
+    print(num1%num2)
+    break
---
+++ 18108.py
@@ -0,0 +1,4 @@
+year=int(input())
+while year>=1000&year<=3000:
+    print(year-543)
+    break
